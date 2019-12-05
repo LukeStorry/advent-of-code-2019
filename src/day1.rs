e32fa344c682227f6085603d277b16d16888eb49
Luke Storry <Luke.Storry@ghyston.com>
2019-12-05 17:48:31 +0000

    moved to using cargo

--- src/day1.rs
+++ src/day1.rs
@@ -1,7 +1,7 @@
 use std::fs::read_to_string;
 
 fn get_input() -> Vec<i32> {
-    read_to_string("input1.txt")
+    read_to_string("input/1.txt")
         .unwrap()
         .trim()
         .split('\n')
@@ -18,13 +18,14 @@ fn calc_fuel_part2(mass: &i32) -> i32 {
     fuel + (if fuel <= 0 { 0 } else { calc_fuel_part2(&fuel) })
 }
 
-fn main() {
-    let input = get_input();
+pub fn part1() {
+    let total_fuel_needed: i32 = get_input().iter().map(calc_fuel_part1).sum();
+    print!("Day 1, Part 1: {}\n", total_fuel_needed);
+}
 
-    let total_fuel_part1: i32 = input.iter().map(calc_fuel_part1).sum();
-    let total_fuel_part2: i32 = input.iter().map(calc_fuel_part2).sum();
-
-    print!("{}\n{}", total_fuel_part1, total_fuel_part2);
+pub fn part2() {
+    let total_fuel_needed: i32 = get_input().iter().map(calc_fuel_part2).sum();
+    print!("Day 1, Part 2: {}\n", total_fuel_needed);
 }
 
 
