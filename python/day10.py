2e6c20ac9f352609d3e41720963bf47e8f020283
Luke Storry <Luke.Storry@ghyston.com>
2019-12-10 13:00:26 +0000

    list comprehension - heavy python solution

--- python/day10.py
+++ python/day10.py
@@ -1,35 +1,17 @@
 import math
 from collections import defaultdict
 
-asteroids = [(x, y)
-             for y, line in enumerate(open('./input.txt').read().split('\n'))
-             for x, char in enumerate(line) if char == "#"]
+asteroids = [(x, y) for y, line in enumerate(open('../input/10.txt').read().split('\n')) for x, char in enumerate(line)
+             if char == "#"]
+
+# Part 1
+print(max(map(lambda a: len(set(math.atan2(a[1] - y, a[0] - x) for x, y in asteroids)), asteroids)))
 
 
-def angle(a, b):
-    return -math.atan2(b[0] - a[0], b[1] - a[1])
+# Part 2
+def angle(a, b): return -math.atan2(b[0] - a[0], b[1] - a[1])
 
-
-def detections(a):
-    return len(set(angle(a, b) for b in asteroids))
-
-
-best_location = max(asteroids, key=detections)
-
-print("Part 1: ", detections(best_location))
-
-
-def distance(a, b):
-    return math.sqrt((a[0] - b[0])**2 + (a[1] - b[1])**2)
-
-
-targets = defaultdict(list)
-for a in asteroids:
-    targets[angle(best_location, a)].append(a)
-
-hits = [
-    min(targets[angle], key=lambda a: distance(best_location, a))
-    for angle in sorted(targets.keys())
-]
-
-print("Part2: ", hits[199][0] * 100 + hits[199][1])
+location = max(asteroids, key=lambda a: len(set(angle(a, b) for b in asteroids)))
+hits = [min(asts, key=lambda a: math.sqrt((location[0] - a[0]) ** 2 + (location[1] - a[1]) ** 2))
+        for angle, asts in sorted(defaultdict(list, [(angle(location, a), [a]) for a in asteroids]).items())]
+print(hits[199][0] * 100 + hits[199][1])
