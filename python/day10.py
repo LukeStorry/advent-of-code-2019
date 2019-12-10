0bf6ce35b77a5e0323889b2ffce355f3651cc396
Luke Storry <luke@lukestorry.co.uk>
2019-12-10 10:18:44 +0000

    finished day10

--- python/day10.py
+++ python/day10.py
@@ -1,7 +1,35 @@
-asteroid_locations = [(x,y) for y, line in enumerate(open('./input.txt').read().split('\n')) for x, char in enumerate(line) if char=="#"]
+import math
+from collections import defaultdict
+
+asteroids = [(x, y)
+             for y, line in enumerate(open('./input.txt').read().split('\n'))
+             for x, char in enumerate(line) if char == "#"]
+
+
+def angle(a, b):
+    return -math.atan2(b[0] - a[0], b[1] - a[1])
+
 
 def detections(a):
-    return len(set(math.atan2(a[1]-y,a[0]-x) for x,y in asteroid_locations))
+    return len(set(angle(a, b) for b in asteroids))
 
-print(max(map(detections, asteroid_locations)))
 
+best_location = max(asteroids, key=detections)
+
+print("Part 1: ", detections(best_location))
+
+
+def distance(a, b):
+    return math.sqrt((a[0] - b[0])**2 + (a[1] - b[1])**2)
+
+
+targets = defaultdict(list)
+for a in asteroids:
+    targets[angle(best_location, a)].append(a)
+
+hits = [
+    min(targets[angle], key=lambda a: distance(best_location, a))
+    for angle in sorted(targets.keys())
+]
+
+print("Part2: ", hits[199][0] * 100 + hits[199][1])
