e08e6527eec712e8ecb7c43a2e503e68b05abc4c
Luke Storry <Luke.Storry@ghyston.com>
2019-12-16 19:37:09 +0000

    done day14part1

--- python/day14.py
+++ python/day14.py
@@ -4,7 +4,19 @@ reactions = open('../input/14.txt').read().split('\n')
 recipes = {output[1]: (output[0], ingredients) for *ingredients, output in
            [[(int(x), y) for x, y in re.findall(r'([0-9]+) ([A-Z]+)', reaction)]
             for reaction in reactions]}
-stock = {ingredient: 0 for ingredient in recipes}
 
-print(recipes)
-print(stock)
+requirements = {'ORE': 0, 'FUEL': 1}
+while True:
+    remaining = [r for r in requirements if r != 'ORE' and requirements[r] > 0]
+    if not remaining:
+        break
+
+    for chemical in remaining:
+        (output_quantity, recipe) = recipes[chemical]
+
+        for (quantity, ingredient) in recipe:
+            requirements[ingredient] = requirements.get(ingredient, 0) + quantity
+
+        requirements[chemical] -= output_quantity
+
+print(requirements['ORE'])
